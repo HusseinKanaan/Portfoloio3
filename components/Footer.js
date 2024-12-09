993ce2d7beb0a16be1488548d63a209046ebb2b2
EXPLODEVID <explodevid@gmail.com>
2024-12-09 12:33:02 +0100

    email confi,

--- components/Footer.js
+++ components/Footer.js
@@ -8,44 +8,25 @@ export default function Footer() {
     return (
         <footer className={styles.footer}>
             <div className={styles.footerContainer}>
-                {/* Typewriter oben in der Mitte */}
-{/*                 <div className={styles.typewriterContainer}>
-                    <Typewriter />
-                </div>  */}
-
                 {/* Branding und Beschreibung */}
                 <div id="contact" className={styles.footerTop}>
                     <div>
-                        {/*                         <Link href="/" className="navbar-brand">
-                            <Image priority={true} src="/logo4.png" alt="Logo" width={100} height={100} className="img-fluid max-width-100" />
-                            <span className="logoSpan" id="logoSchrift"></span>
-                        </Link> */}
                         <h1 className={styles.footerLogo}>Hussein Kanaan</h1>
-
                         {/* E-Mail und Telefon Icons */}
                         <div className={styles.contactIcons}>
                             <i className=""></i> E-Mail: husseinkanaan255@gmail.com
                             <br />
                             <i className=""></i> Telefon: +49 157 343 66370
                         </div>
-
                         <p className={styles.footerDescription}>
                             Innovatives Webdesign, das Marken auf die nächste Ebene bringt. Wir schaffen digitale Erlebnisse, die beeindrucken.
                         </p>
                     </div>
-
                     {/* Newsletter Sektion */}
                     <div /* className={styles.newsletter} */>
                         <ContactForm />
-
-                        {/*                         </div>                        <h5>In Verbindung bleiben</h5>
-                        <p>Abonnieren Sie unseren Newsletter, um die neuesten Updates zu erhalten.</p>
-                        <div className="d-flex">
-                            <input type="email" placeholder="Ihre E-Mail-Adresse" />
-                            <button>Abonnieren</button> */}
                     </div>
                 </div>
-
                 {/* Navigationslinks */}
                 <div className={styles.footerLinks}>
                     <div className={styles.footerColumn}>
@@ -56,7 +37,6 @@ export default function Footer() {
                             <li><a href="#">Technologien</a></li>
                         </ul>
                     </div>
-
                     <div className={styles.footerColumn}>
                         <h5>Informationen</h5>
                         <ul>
@@ -65,7 +45,6 @@ export default function Footer() {
                             <li><a href="#">FAQ</a></li>
                         </ul>
                     </div>
-
                     <div className={styles.footerColumn}>
                         <h5>Rechtliches</h5>
                         <ul>
@@ -75,7 +54,6 @@ export default function Footer() {
                         </ul>
                     </div>
                 </div>
-
                 {/* Social Media und Bottom Section */}
                 <div className={styles.footerBottom}>
                     <div className={styles.socialIcons}>
